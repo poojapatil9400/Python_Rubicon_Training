1143141108f4b0f91310a47c43acf98c4a626626
Pooja9400 <poojapatil121076@gmail.com>
2025-07-25 15:47:11 +0530

    day4

--- Day_4/inherit.py
+++ Day_4/inherit.py
@@ -1,4 +1,5 @@
 #Inheritance
+#single inheritance 
 class Rectangle:
     def rec_are(self,height,width):
         area = height*width
@@ -10,3 +11,21 @@ class Square(Rectangle):
 obj= Square()
 obj.rec_are(10,20)
 obj.squ_area(10)
+
+#multiple inheritance 
+class demo:
+    def student(self,name):
+        print("Name",name)
+class demo1:
+    def studata(self, email):
+        print("Email",email)
+class demo2(demo,demo1):
+
+    def age(self, age):
+     print("age",age)
+obj = demo2()
+obj.student("Pooja")
+obj.studata("Pooja@1210")
+obj.age(20)
+
+
